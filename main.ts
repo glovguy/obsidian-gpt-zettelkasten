9bad65e4658bf74cf2bb3ab2fd9be0473364dc6f
Karl <karlsmith@bouzou.com>
2023-07-25 22:55:18 -0400

    Fixes up README

--- main.ts
+++ main.ts
@@ -1,16 +1,13 @@
 import { 
 	App, 
-	Editor, 
 	MarkdownView, 
-	Modal, 
-	Notice, 
 	Plugin, 
 	PluginSettingTab, 
 	Setting,
 	TFile
 } from 'obsidian';
 import { initOpenAI, generateAndStoreEmbeddings } from './src/semantic_search';
-import { VectorStore, LocalVectorDict, StoredVector } from './src/vector_storage';
+import { VectorStore, StoredVector } from './src/vector_storage';
 import SemanticSearchModal from './src/semantic_search_modal';
 import BatchVectorStorageModal from './src/batch_vector_storage_modal';
 
@@ -37,22 +34,14 @@ export default class MyPlugin extends Plugin {
 		await this.loadSettings();
 		this.vectorStore = new VectorStore(this);
 
-		// // This creates an icon in the left ribbon.
-		// const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-		// 	// Called when the user clicks the icon.
-		// 	new Notice('This is a notice!');
-		// });
-		// // Perform additional things with the ribbon
-		// ribbonIconEl.addClass('my-plugin-ribbon-class');
-
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		// const statusBarItemEl = this.addStatusBarItem();
+		// statusBarItemEl.setText('Status Bar Text');
 
-		// Generate embeddings current note command
+		// Generate embeddings for current note command
 		this.addCommand({
 			id: 'generate-embeddings-current-note',
-			name: 'Generate embeddings for current note only',
+			name: 'Generate embeddings for current note',
 			callback: async () => {
 				const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
 				if (activeView) {
@@ -74,7 +63,7 @@ export default class MyPlugin extends Plugin {
 		// Semantic search command
 		this.addCommand({
 			id: 'open-semantic-search-modal',
-			name: 'Semantic Search',
+			name: 'Semantic Search for notes similar to current note',
 			callback: () => {
 				new SemanticSearchModal(this.app, this).open();
 			}
@@ -88,35 +77,6 @@ export default class MyPlugin extends Plugin {
 				new BatchVectorStorageModal(this.app, this).open();
 			}
 		});
-
-		// This adds an editor command that can perform some operation on the current editor instance
-		// this.addCommand({
-		// 	id: 'sample-editor-command',
-		// 	name: 'Sample editor command',
-		// 	editorCallback: (editor: Editor, view: MarkdownView) => {
-		// 		console.log(editor.getSelection());
-		// 		editor.replaceSelection('Sample Editor Command');
-		// 	}
-		// });
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		// this.addCommand({
-		// 	id: 'open-sample-modal-complex',
-		// 	name: 'Open sample modal (complex)',
-		// 	checkCallback: (checking: boolean) => {
-		// 		// Conditions to check
-		// 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-		// 		if (markdownView) {
-		// 			// If checking is true, we're simply "checking" if the command can be run.
-		// 			// If checking is false, then we want to actually perform the operation.
-		// 			if (!checking) {
-		// 				new SampleModal(this.app).open();
-		// 			}
-
-		// 			// This command will only show up in Command Palette when the check function returns true
-		// 			return true;
-		// 		}
-		// 	}
-		// });
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
