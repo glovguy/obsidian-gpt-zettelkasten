2399ad87d8de5305209f8af164dfc2fcb756228d
Karl <karlsmith@bouzou.com>
2023-07-31 13:58:49 -0400

    Removes debounce

--- src/utils.ts
+++ src/utils.ts
@@ -4,11 +4,3 @@ import { SHA256, enc } from 'crypto-js';
 export const shaForString = function(str: string): string {
   return enc.Base64.stringify(SHA256(str));
 };
-
-export const debounce = function(func: () => void, timeout = 300) {
-  let timer: NodeJS.Timeout;
-  return (...args: any[]) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => { func.apply(this, args); }, timeout);
-  };
-};
