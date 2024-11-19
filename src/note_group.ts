1beea7df63b82ab5238f737c6ad424a5c26d4980
Karl <karlsmith@bouzou.com>
2024-11-18 23:44:20 -0500

    Removes default literature notes config

--- src/note_group.ts
+++ src/note_group.ts
@@ -4,10 +4,6 @@ export const DEFAULT_NOTE_GROUPS: Array<NoteGroup> = [{
   name: "Permanent Notes",
   notesFolder: null,
   copilotPrompt: 'The following is a Zettelkasten note written by the user. The note should have 1. a clear title, 2. a single, clear thought stated briefly, 3. links to relevant ideas.\nSuggest revisions for this note. Be very brief and concise. Imitate their writing style. If you show an example of the suggested edits, wrap them in a <note></note> tag. If you want to suggest splitting into multiple notes, use more than one <note></note> tag.',
-},{
-  name: "Literature Notes",
-  notesFolder: null,
-  copilotPrompt: 'The following is a Zettelkasten note written by the user. The note should have 1. a clear title, 2. a single, clear thought stated briefly, 3. links to relevant ideas.\nSuggest revisions for this note. Be very brief and concise. Imitate their writing style. If you show an example of the suggested edits, wrap them in a <note></note> tag. If you want to suggest splitting into multiple notes, use more than one <note></note> tag.',
 }];
 
 export interface NoteGroup {
